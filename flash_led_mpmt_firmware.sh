dea141dac7fd95c35155c71cfe696e6261a2568b
Arturo Fiorentini <arturo.fiorentini@gmail.com>
2026-06-15 17:56:39 -0400

    update LED-mPMT firmware version to flash

--- flash_led_mpmt_firmware.sh
+++ flash_led_mpmt_firmware.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-# two comments (Razvan)
-# the echo command can be run from any directory, no need to cd into /lib/firmware
-# we should change the firmware name to samething that has LED-mPMT in the main, LED-FEB is the light emitter
-
-# previously used commands
-#echo LED-mPMT-firmware-v3-25-09-24.bit.bin > /sys/class/fpga_manager/fpga0/firmware
-#echo "10MHz-trig-aux-out.bin" > /sys/class/fpga_manager/fpga0/firmware
-#echo LED-mPMT-firmware-v3-25-10-09.bit.bin > /sys/class/fpga_manager/fpga0/firmware
-#echo LED-mPMT-firmware-v3-25-10-23.bit.bin > /sys/class/fpga_manager/fpga0/firmware
-#echo LED-mPMT-firmware-v3-25-10-30.bit.bin > /sys/class/fpga_manager/fpga0/firmware
-
-# current version
-echo LED-mPMT-firmware-v3-25-12-04.bit.bin > /sys/class/fpga_manager/fpga0/firmware
+echo LED-mPMT-firmware-v3-26-04-17.bin > /sys/class/fpga_manager/fpga0/firmware
 rmmod dma-proxy
 modprobe dma-proxy
 dmesg | tail -n 6
